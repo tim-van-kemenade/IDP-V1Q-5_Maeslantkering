4159b8a7fab43dfacf7bcb535ecaa627bae9ad32
Tim van Kemenade <tim.vankemenade@student.hu.nl>
2018-01-24 15:12:47 +0100

    Add repository to save data to database on the server

---
+++ server/repository/repository.py
@@ -0,0 +1,79 @@
+import sqlite3
+import time
+
+
+class WaterRepository:
+    def __init__(self):
+        self.connection = sqlite3.connect('Weather.db', check_same_thread=False)
+        self.create_table()
+
+    def create_table(self):
+        self.connection.execute(
+            'CREATE TABLE IF NOT EXISTS water ('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'average_height REAL,'
+            'epoch REAL'
+            ');'
+        )
+        self.connection.commit()
+
+    def add_data(self, water_height):
+        self.connection.execute('INSERT INTO water (average_height, epoch)'
+                                'VALUES({}, {});'.format(water_height,
+                                                         time.time()
+                                                         )
+                                )
+        self.connection.commit()
+
+    def fetch_data(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM water ORDER BY id DESC LIMIT 42')
+        return cursor.fetchall()
+
+
+class StormRepository:
+    def __init__(self):
+        self.connection = sqlite3.connect('Weather.db', check_same_thread=False)
+        self.create_table()
+
+    def create_table(self):
+        self.connection.execute(
+            'CREATE TABLE IF NOT EXISTS storm ('
+               'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+               'windsnelheidMS REAL,'
+               'windrichtingGR INTEGER,'
+               'windstotenMS REAL,'
+               'epoch REAL'
+               ');'
+        )
+        self.connection.commit()
+
+    def add_data(self, wind_speed, wind_direction, wind_burst):
+        self.connection.execute('INSERT INTO storm (windsnelheidMS, windrichtingGR, windstotenMS, epoch)'
+                                'VALUES({}, {}, {}, {});'.format(wind_speed,
+                                                                 wind_direction,
+                                                                 wind_burst,
+                                                                 time.time()
+                                                                 )
+                                )
+        self.connection.commit()
+
+    def fetch_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM storm ORDER BY id DESC')
+        return cursor.fetchall()
+
+    def fetch_wind_burst(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT id, windstotenMS FROM storm ORDER BY id DESC LIMIT 42')
+        return cursor.fetchall()
+
+    def fetch_wind_speed(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT id, windsnelheidMS FROM storm ORDER BY id DESC LIMIT 42')
+        return cursor.fetchall()
+
+    def fetch_current_direction(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT windrichtingGR FROM storm ORDER BY id DESC')
+        return cursor.fetchone()
