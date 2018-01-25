b56b6264d4ae047d6ba0b126de3a4073593d057e
Tim van Kemenade <tim.vankemenade@student.hu.nl>
2018-01-25 16:14:18 +0100

    Added states for closing and opening the barrier

--- server/states/closing_state.py
+++ server/states/closing_state.py
@@ -1,11 +1,11 @@
 from .state_interface import StateInterface
 
 
-class OpeningState(StateInterface):
+class ClosingState(StateInterface):
     def __init__(self, client, hardware):
         self.client = client
         self.hardware = hardware
 
     def handle(self):
-        self.hardware.open_gate()
-        return 'open'
+        self.hardware.close_gate()
+        return 'closed'
